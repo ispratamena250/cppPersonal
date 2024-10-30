5b3cb2bef20a99050404424742a926de51dd7174
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-10-30 08:33:33 -0300

    Commit com tentativa de exercício da lista 1 de TEP

---
+++ codigosTEP/hq9plus.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	int count = 0, aux;
+	string s;
+
+	cin >> s;
+	aux = s.size();
+	for(int i=0; i<aux; i++){
+		if(s[i] == 'H' || s[i] == 'Q' || s[i] == '9' || s[i] == '+'){
+			count++;
+		}
+	}
+	if(count != 0){
+		cout << "YES" << endl;
+	}else{
+		cout << "NO" << endl;
+	}
+
+	return 0;
+}
