46a6149ebe417c72b8f29d3aacf0f4c96c840565
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-11-08 21:56:04 -0300

    Commit com código do vetor com indices cíclicos completo

--- avulsos/vetorRepetindo10em10.cpp
+++ avulsos/vetorRepetindo10em10.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include <vector>
 
+#define NUM 50
+
 using namespace std;
 
 int main(){
-	vector<int> v(20);
+	vector<int> v(NUM);
 	int count=0;
 
-	for(int i=0; i<20; i++){
-		if(count<=9){
-			v[i] = count;
-			count++;
-		}else{
-			count=0;
-		}	
+	for(int i=0; i<NUM; i++){
+		if(i == 0){
+			v[0] = 0;
+		}else if(i >= 1){
+			if(count<9){
+				count++;
+				v[i] = count;
+			}else{
+				count=0;
+				v[i] = count;
+			}	
+		}
 	}
-	for(int i=0; i<20; i++){
+	for(int i=0; i<NUM; i++){
 		cout << v[i] << " ";
 	}
 	cout << endl;
