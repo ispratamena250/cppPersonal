9296f832f35a7ba9db894ceaceb290308b58a771
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-11-17 21:32:43 -0300

    Commit com exercício 10 funcionando

--- estudoStrings/ex10.cpp
+++ estudoStrings/ex10.cpp
@@ -5,17 +5,18 @@ using namespace std;
 
 int main(){
 	string s;
-	int len;
+	int len, count;
 
-	cin >> s;
+	//cin >> s;
+	getline(cin, s); //Lê toda a linha, incluindo espaços
 	len = s.size();
+	count = s.size();
 
 	for(int i=0; i<len; i++){
-		while(i <= len-1){
-			cout << s[i] << endl;
-			i--;
-		}
+		cout << s[count-1];
+		count--; 
 	}
+	cout << endl;
 
 	return 0;
 }
