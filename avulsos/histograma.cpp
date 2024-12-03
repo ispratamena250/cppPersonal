654a6aed9780b687663650b34f8a0615a39452b4
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-12-03 15:45:07 -0300

    Commit com código para implementar um histograma para strings em C++

---
+++ avulsos/histograma.cpp
@@ -0,0 +1,39 @@
+/*
+* Código que implementa um histograma de strings em C++
+*/
+
+#include <iostream>
+#include <string>
+#include <map>
+//#include <bits/stdc++.h>
+
+using namespace std;
+
+//Assinatura
+map<char, int> hist(const string& s);
+
+int main(int argc, char* argv[2]){
+	if(argc != 2){
+		cout << "Erro! Compile assim: $ ./histograma s" << endl;
+		exit(1);
+	}
+
+	string s = argv[1];
+	cout << s << endl;
+	//hist(s); //Errado
+	//cout << hist(s) << endl; //Errado
+	map<char, int> h = hist(s);
+	for(const auto& c : h)
+		cout << c.first << ":" << c.second << endl;
+
+	return 0;
+}
+
+map<char, int> hist(const string& s){
+	map<char, int> h;
+
+	for(auto c : s)
+		++h[c];
+
+	return h;
+}
