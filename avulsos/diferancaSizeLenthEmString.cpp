db1b61974110daae46f39222c8812ee16ea52fd5
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-12-05 15:49:41 -0300

    Commit com pensamentos sobre strings

---
+++ avulsos/diferancaSizeLenthEmString.cpp
@@ -0,0 +1,16 @@
+/*
+* A diferença é apenas estilística
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string a = "casa";
+
+	cout << a.size() << " "  << a.length() << endl;
+
+	return 0;
+}
