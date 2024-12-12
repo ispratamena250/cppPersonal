7763e856865d53bac312266ae766865216fa5841
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-12-12 15:51:31 -0300

    Commit com código ainda inacabado

---
+++ codeForces/wordCapitalization.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+	ios::sync_stdio(false);	
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	string s;
+	for(size_t i=0; i<s.size(); i++){
+		if(i==0){
+			cout << s[i] + '32';
+		}
+			cout << s[i];
+	}
+	cout << endl;
+
+	return 0;
+}
