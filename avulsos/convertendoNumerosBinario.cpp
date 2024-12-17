7e82249cacb387cf78bc9a04e928226302677179
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-12-17 14:26:43 -0300

    Commit com início do trabalho de curvas elípticas

---
+++ avulsos/convertendoNumerosBinario.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bitset>
+
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+
+	cout << bitset<20>(n) << endl;
+	//Terminar de pensar nisso depois.	
+
+	return 0;
+}
