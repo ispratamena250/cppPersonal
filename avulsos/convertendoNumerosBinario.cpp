bcbc87a6b7d7848bbe70e10c97f88de69ceb6175
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-12-17 15:38:19 -0300

    Commit com ideias sobre o trabalho de curvas elípticas

--- avulsos/convertendoNumerosBinario.cpp
+++ avulsos/convertendoNumerosBinario.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 #include <bitset>
+#include <math.h>
+#include <vector>
 
 using namespace std;
 
 int main(){
-	int n;
+	int n, aux=2;
+	vector<int> v;
 	cin >> n;
 
 	cout << bitset<20>(n) << endl;
-	//Terminar de pensar nisso depois.	
+
+	for(int i=0; i<20; i++){
+		cout << pow(aux, i) << " ";
+		v.push_back(pow(aux, i));
+	}
+
+	cout << endl << n << " " << v.size() << endl << "Inicio:" << endl;
+	for(size_t i=0; i<v.size(); i++){
+		if(n < v[i]){
+			cout << i;
+			const int pos = i;
+			break;
+		}
+	}
+	
+	cout << bitset<pos>(n) << endl;	
 
 	return 0;
 }
