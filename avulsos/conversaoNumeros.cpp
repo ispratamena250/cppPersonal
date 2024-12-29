5f809d5395c19fb812797c5c741cf049113eac9d
IsaacMPereira <isaacp.menezesp@gmail.com>
2024-12-28 21:37:19 -0300

    Commit com vários pensamentos sobre conversão de números até descobrir a lib GMP

--- avulsos/conversaoNumeros.cpp
+++ avulsos/conversaoNumeros.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <bitset>
+#include <climits>
 
 using namespace std;
 
 int main(){
-	int n = 720;
+	unsigned long long n = 681684165198;
+  unsigned long long n2 = ULLONG_MAX;
+  cout << n2 << endl;
 
 	//cout << dec << n << endl;
 	//cout << hex << n << endl;
 	//cout << oct << n << endl;
-	cout << bitset<20>(n) << endl;
+	cout << bitset<100>(n) << endl << endl;
+  cout << bitset<100>(n2) << endl;
 
 	return 0;
 }
--- avulsos/conversorBinDec.cpp
+++ avulsos/conversorBinDec.cpp
@@ -4,23 +4,31 @@
 
 using namespace std;
 
-int main(){
+int main(int argc, char *argv[2]){
+  if(argc != 2){
+    cout << "Erro! Compile assim: $ ./conversorBinDec n" << endl;
+    exit(1);
+  }
+
 	vector<int> v;
 
-	long long n;
+	long long n = atoi(argv[1]);
 	int div, q;
-	cin >> n;
-
 	div = n;
 	while(q > 1){
 		while(div >= 1 || div >= 0){
 			div -= 2;
+      //cout << "div: " << div << endl;
 		}		
 		v.push_back(div+2);
+    //cout << "div+2: " << div+2 << endl;
 		q = (n - (div+2))/2;
+    //cout << "q: " << q << endl;
 		
 		n = q;
 		div = n;
+    //cout << "n: " << q << endl;
+    //cout << "div(ultima): " << div << endl;
 	}
 	v.push_back(q);
 
@@ -30,6 +38,5 @@ int main(){
 		cout << e;
 
 	cout << endl;
-
 	return 0;
 }
