75b78595c5dc90c13745ddf2c589f96e124760b3
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-01-07 15:49:03 -0300

    Commit com tentativas frustradas em leetCode

--- codeForces/d900/dubStep.cpp
+++ codeForces/d900/dubStep.cpp
@@ -2,18 +2,42 @@
 
 using namespace std;
 
+int occurences_changedBitStep(const string& P, const string& S);
+
 int main(){
   string a, b = "WUB";
   cin >> a;
 
-  size_t goalPos = a.find(b);
+  cout << occurences_changedBitStep(b, a) << endl; 
 
-  for(size_t i=0; i<a.size(); i++){
-    if(i < goalPos || i >= goalPos + b.size()){
-      cout << a[i];
+  cout << endl;
+  return 0;
+}
+
+int occurences_changedBitStep(const string& P, const string& S){
+  size_t m = P.size();
+  size_t n = S.size();
+  int occ=0;
+  vector<int> v;
+
+  for(size_t i=0; i<=n-m; i++){
+    if(P == S.substr(i, m)){
+      occ++;
+      //cout << i << endl;
+      v.push_back(i);
+    }
+  }
+
+  for(size_t i=0; i<S.size(); i++){
+    for(size_t j=0; j<v.size(); j++){
+      size_t aux = v[j];
+      if(i < aux or i >= aux + m){
+        cout << S[i];
+        break;
+      }
     }
   }
 
   cout << endl;
-  return 0;
+  return occ;
 }
