79971a06e85b4b1ec00eec94ada75d1e45c12867
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-01-18 22:02:18 -0300

    Commit com pensamentos sobre Rabin Karp

--- avulsos/strings/hashing/rabinKarp1.cpp
+++ avulsos/strings/hashing/rabinKarp1.cpp
@@ -1,29 +1,23 @@
 #include <bits/stdc++.h>
 
-int f(char c)
-{
-    return c - 'a' + 1;
+int rabin_karp(const std::string& s, const std::string& p);
+int h(const std::string& s);
+int f(char c);
+
+int main(){
+    auto s = "ababababababa", p = "aba";
+    auto s1 = "trespratosdetrigoparatrestigrestristes", p1 = "tr";
+
+    std::cout << rabin_karp(s, p) << std::endl;
+    std::cout << rabin_karp(s1, p1) << std::endl;
+
+    return 0;
 }
 
-int h(const std::string& s)
-{
-    long long ans = 0, p = 31, q = 1000000007;
-
-    for (auto it = s.rbegin(); it != s.rend(); ++it)
-    {
-        ans = (ans * p) % q;
-        ans = (ans + f(*it)) % q;
-    }
-
-    return ans;
-}
-
-int rabin_karp(const std::string& s, const std::string& p)
-{
+int rabin_karp(const std::string& s, const std::string& p){
     int n = s.size(), m = p.size(), occ = 0, hp = h(p); 
 
-    for (int i = 0; i <= n - m; i++)
-    {
+    for (int i = 0; i <= n - m; i++){
         auto b = s.substr(i, m);
         occ += (h(b) == hp && b == p) ? 1 : 0;
     }
@@ -31,11 +25,17 @@ int rabin_karp(const std::string& s, const std::string& p)
     return occ;
 }
 
-int main()
-{
-    auto s = "ababababababa", p = "aba";
+int h(const std::string& s){
+    long long ans = 0, p = 31, q = 1000000007;
 
-    std::cout << rabin_karp(s, p) << '\n';
+    for (auto it = s.rbegin(); it != s.rend(); ++it){
+        ans = (ans * p) % q;
+        ans = (ans + f(*it)) % q;
+    }
 
-    return 0;
+    return ans;
+}
+
+int f(char c){
+    return c - 'a' + 1;
 }
