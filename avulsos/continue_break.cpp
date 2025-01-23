c86d19ceed45330b17a68ff018bad9702d4d7a69
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-01-23 15:50:17 -0300

    Commit com descobrimento da palavra 'continue'

---
+++ avulsos/continue_break.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	for (int i=0; i<5; i++){
+		if(i==3)
+			break;
+		cout << i << " ";
+	}
+
+	cout << endl;
+	for(int i=0; i<5; i++){
+		if(i==3)
+			continue;
+
+		cout << i << " ";
+	}
+
+	cout << endl;
+	return 0;
+}
