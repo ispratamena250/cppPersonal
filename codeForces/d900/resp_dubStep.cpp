4b534e9941219e1a69764930fb2fa05efe1cbd8c
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-01-23 15:50:36 -0300

    Commit com pensamentos sobre o problema

--- codeForces/d900/resp_dubStep.cpp
+++ codeForces/d900/resp_dubStep.cpp
@@ -1,21 +1,48 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 int main() {
-	string s;
-	int flag=1;
-	cin>>s;
-	for(int i=0;i<s.size();i++){
-	    if(s[i]=='W'&& s[i+1]=='U' && s[i+2]=='B'){
-	        i+=2;
-	        if(!flag){
-	            cout<<" ";
-	        }
-	        continue;
-	    }else {
-	        flag=0;
-	        cout<<s[i];
-	    }
-	}
-	return 0;
+  string s;
+  int flag=1;
+  vector<int> v, v2, v2i, v3;
+
+  cin >> s;
+
+  for(size_t i=0; i<s.size(); i++){
+    if(s[i]=='W' and s[i+1]=='U' and s[i+2]=='B'){
+      v2i.push_back(i);
+      i+=2;
+      v2.push_back(i);
+      if(!flag){
+        cout << " ";
+        v.push_back(!flag);
+      }
+      continue;
+    }else {
+      flag=0;
+      cout << s[i];
+    }
+    v3.push_back(i);
+  }
+
+  cout << endl;
+  for(auto e : v)
+    cout << e << " ";
+
+  cout << endl;
+  for(auto e : v2)
+    cout << e << " ";
+
+  cout << endl;
+  for(auto e : v2i)
+    cout << e << " ";
+
+  cout << endl;
+  for(auto e : v3)
+    cout << e << " ";
+
+  cout << endl;
+  return 0;
 }
