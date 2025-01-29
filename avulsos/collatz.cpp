b8b3f82c5a6edca07949d6182103ffdea6ca4e02
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-01-28 21:57:08 -0300

    Commit com melhora do código para análise da Conjectura de Collatz

--- avulsos/collatz.cpp
+++ avulsos/collatz.cpp
@@ -1,29 +1,60 @@
+/*
+ *  C++ program to analise the Collatz Conjecture
+ *  Compile: $ g++ -Wall collatz.cpp -o collatz.x
+ *  Run: $ ./collatz.x z
+ * */
+
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
 
 using namespace std;
 
-int main(){
-  srand(time(NULL));
-  int n = rand()%1000;
-  int aux = n;
+int main(int argc, char *argv[2]){
+  if(argc != 2){
+    cerr << "Error! Run: $ ./collatz.x z" << endl;
+    exit(1);
+  }
+
+  FILE *gnuplotPipe = popen("gnuplot -persistent", "w");
+  if(!gnuplotPipe){
+    cerr << "Error on openning Gnuplot pipe" << endl;
+    exit(1);
+  }
+
+  long long z = atoi(argv[1]);
   int count=0;
 
-  cout << n << endl;
-  while(true){
-    if(n == 1){
-      count++;
-      break;
-    }else if(n%2 == 0){
-      n = n/2;
-      count++;
-      cout << n << endl;
-    }else if(n%2 != 0){
-      n = 3*n + 1;
-      cout << n << endl;
-      count++;
+  ofstream file("dados_collatz.txt");
+
+  if(file.is_open()){
+    while(true){
+      if(z == 1){
+        count++;
+        break;
+      }else if(z%2 == 0){
+        z = z/2;
+        count++;
+        file << count << "\t" << z << endl;
+      }else if(z%2 != 0){
+        z = 3*z + 1;
+        count++;
+        file << count << "\t" << z << endl;
+      }
     }
-  } 
-  cout << "Primeiro: " << aux << " Quantidade: " << count;
+    file.close();
+  }else{
+    cerr << "Error on openning dados_collatz.txt" << endl;
+    exit(1);
+  }
+
+  fprintf(gnuplotPipe, "set xlabel 'Números'\n");
+  fprintf(gnuplotPipe, "set ylabel 'Quantidade de numeros gerados'\n");
+  fprintf(gnuplotPipe, "set title 'Analise da quantidade de numeros geradas na conjectura de Collatz para cada numero'\n");
+  fprintf(gnuplotPipe, "plot 'dados_collatz.txt' u 1:2 w lines lc rgb 'red' notitle\n");
+
+  pclose(gnuplotPipe);
 
   cout << endl;
   return 0;
