ea895bf1bd5248753a88bb26cee1f3767716fa09
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-01-30 15:24:17 -0300

    Commit com pensamentos

--- codeForces/d900/resp_dubStep.cpp
+++ codeForces/d900/resp_dubStep.cpp
@@ -3,45 +3,27 @@
 
 using namespace std;
 
-int main() {
+int main() { //Bom exemplo: WUBABCWUBDEFWUBWUB
   string s;
   int flag=1;
-  vector<int> v, v2, v2i, v3;
 
   cin >> s;
 
   for(size_t i=0; i<s.size(); i++){
+    cout << "flag1: " << flag << endl;
     if(s[i]=='W' and s[i+1]=='U' and s[i+2]=='B'){
-      v2i.push_back(i);
       i+=2;
-      v2.push_back(i);
       if(!flag){
+        cout << "!flag: " << !flag << endl;
         cout << " ";
-        v.push_back(!flag);
       }
       continue;
     }else {
       flag=0;
-      cout << s[i];
+      cout << s[i] << " -> ";
     }
-    v3.push_back(i);
+    cout << "flag2: " << flag << endl;
   }
-
-  cout << endl;
-  for(auto e : v)
-    cout << e << " ";
-
-  cout << endl;
-  for(auto e : v2)
-    cout << e << " ";
-
-  cout << endl;
-  for(auto e : v2i)
-    cout << e << " ";
-
-  cout << endl;
-  for(auto e : v3)
-    cout << e << " ";
 
   cout << endl;
   return 0;
