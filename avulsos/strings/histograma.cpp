66bcbd7a6e314bb6a1d0c7208f52ca1650f7974a
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-02-02 22:26:18 -0300

    Profundos pensamentos sobre a Conjectura de Collatz e sub produtos disso

--- avulsos/strings/histograma.cpp
+++ avulsos/strings/histograma.cpp
@@ -13,12 +13,13 @@ using namespace std;
 map<char, int> hist(const string& s);
 
 int main(int argc, char* argv[2]){
-	if(argc != 2){
+	/*if(argc != 2){
 		cout << "Erro! Compile assim: $ ./histograma s" << endl;
 		exit(1);
-	}
+	}*/
 
-	string s = argv[1];
+	//string s = argv[1];
+  string s = "c a s a";
 	cout << s << endl;
 	//hist(s); //Errado
 	//cout << hist(s) << endl; //Errado
