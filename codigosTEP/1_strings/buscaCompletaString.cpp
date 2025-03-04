abe418d42f738d92ef1c497f54d96c4bcf8b83e6
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-03-04 18:28:05 -0300

    Uso

--- codigosTEP/1_strings/buscaCompletaString.cpp
+++ codigosTEP/1_strings/buscaCompletaString.cpp
@@ -6,24 +6,24 @@ using namespace std;
 int occurences(const string& P, const string& S);
 
 int main(){
-  string S = "Deitado berco eternamente em berco esplendido";
-  string P = "berco";
+	string S = "Deitado berco eternamente em berco esplendido";
+	string P = "berco";
 
-  cout << occurences(P, S) << endl; 
+	cout << occurences(P, S) << endl; 
 
-  cout << endl;
-  return 0;
+	cout << endl;
+	return 0;
 }
 
 int occurences(const string& P, const string& S){
-  int m = P.size();
-  int n = S.size();
-  int occ = 0;
+	int m = P.size();
+	int n = S.size();
+	int occ = 0;
 
-  for(int i=0; i<=n-m; i++){
-    occ += (P == S.substr(i, m) ? 1 : 0);
-    cout << "String: " << S.substr(i, m) << endl;
-  }
+	for(int i=0; i<=n-m; i++){
+		occ += (P == S.substr(i, m) ? 1 : 0);
+		cout << "String: " << S.substr(i, m) << endl;
+	}
 
-  return occ;
+	return occ;
 }
