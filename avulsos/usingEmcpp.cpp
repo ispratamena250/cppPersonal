84ff74d62d66f2767544466675c00912a256a123
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-03-18 15:44:24 -0300

    Tirando dúvida sobre alias

---
+++ avulsos/usingEmcpp.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){ //não.
+	int contoller = 0;
+	using controller = cc;
+
+	for(int i=0; i<10; i++){
+		cout << cc << endl;
+		c++;
+	}
+
+	cout << endl;
+	return 0;
+}
