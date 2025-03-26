20d6b70f4c30010fd16055e831f817f3f4f04b0c
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-03-26 08:31:44 -0300

    Quase lá

--- codeForces/d800/nightAtTheMuseum.cpp
+++ codeForces/d800/nightAtTheMuseum.cpp
@@ -1,55 +1,25 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 int main(){
-	string s, alfa1, alfa2;
-	char start = 'a';
+	char begin, end;
 
-	cin >> s;
+	cin >> begin >> end;
 
-	//Gerando
-	for(int i=start; i<='z'; i++){
-		alfa1.push_back(i);
+	int count1=0;
+	for(int i=begin; i<=end; i++){
+		count1++;
+		count1 = count1 % 26;
 	}
-	for(int i='z'; i>=start; i--){
-		alfa2.push_back(i);
-	}
+	cout << count1-1 << endl;
 
-	int count=1;
-	for(auto e : alfa1){
-		cout << e << count << " "; //horarios
-		count++;
+	int count2=0;
+	for(int i=end; i<=begin; i--){
+		count2++;
+		count2 = count2 % 26;
 	}
-	cout << endl;
-	count=26;
-	for(auto e : alfa2){
-		cout << e << count << " "; //anti horario
-		count--;
-	}
-	cout << endl;
-
-	for(size_t i=0; i<s.size(); i++){
-		int count=1;
-		for(auto e : alfa1){
-			if(s[i] == e){
-				cout << "h  -> " << s[i] << count << endl;
-			}
-			count++;
-		}
-		count=26;
-		for(auto f : alfa2){
-			if(s[i] == f){
-				cout << "ah -> " << s[i] << count << endl;
-			}
-			count--;
-		}			
-	}
-
-	//cout << p2 - p1 << endl; //Aqui
-
-	//cout << p1 + (26 - p2) << endl; //Aqui
+	cout << count2+1 << endl;
 
 	cout << endl;
 	return 0;
