162c11b81c058746d0f7408607db96068de9be80
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-04-20 21:00:45 -0300

    Atualizacoes e coisas

---
+++ avulsos/def_ipp_ppi.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	for(int i=0; i<3; i++)
+		cout << i << endl;
+	
+	cout << endl;
+	for(int i=0; i<3; ++i) 
+		cout << i << endl;
+
+	cout << endl;
+	return 0;
+}
---
+++ avulsos/difErro_apiCpp_codigoProprio.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int z;
+
+	cin << z;
+
+	cout << z << endl;
+
+	cout << endl;
+	return 0;
+}
