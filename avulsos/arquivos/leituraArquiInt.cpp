7dea2a177690bd97d4b180a0aa4a36a14c2168c9
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-04-25 21:59:32 -0300

    Uso

--- avulsos/arquivos/leituraArquiInt.cpp
+++ avulsos/arquivos/leituraArquiInt.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <fstream>
 
+using namespace std;
+
 int main() { //Exemplo de dados: 1 2 3 4 5
-    std::ifstream arquivo("numeros.txt");
+    ifstream arquivo("numeros.txt");
     if (!arquivo.is_open()) {
-        std::cerr << "Erro ao abrir o arquivo!" << std::endl;
+        cerr << "Erro ao abrir o arquivo!" << endl;
         return 1;
     }
 
     int numero;
     while (arquivo >> numero) { // Lê números do arquivo
-        std::cout << "Número lido: " << numero << std::endl;
+        cout << "Número lido: " << numero << endl;
+	getchar();
     }
 
     arquivo.close();
