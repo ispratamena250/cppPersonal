322890b632b20659ab6065fb0e7c70a94d104f24
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-05-12 22:33:09 -0300

    Tamanho de string

---
+++ avulsos/strings/stringSize.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[2]){
+	string s = argv[1];
+
+	cout << s.size() << endl;
+
+	cout << endl;
+	return 0;
+}
