bf392a923fc183de85b36fe554dd4a179b2c9ada
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-06-03 17:10:18 -0300

    Implementacao complexa dos numeros de Fibonacci

---
+++ avulsos/fibonacci_2powern.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int f(int n){
+	return n <= 1 ? n : f(n-1) + f(n-2);
+}
+
+int main(){
+	int n = 50;
+
+	for(int i=0; i<=n; i++){
+		cout << f(i) << " ";
+	}
+
+	cout << endl;
+	return 0;
+}
