e4320ffa551941dfe6e93919f8fe68af88d7b7de
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-07-02 23:19:55 -0300

    Começo da lista de DP

---
+++ codigosTEP/listas/2_paradigmas/ilyaAndQueries_TLE.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+	string s;
+	int m, count=0;
+
+	cin >> s >> m;
+
+	for(int i=0; i<m; i++){
+		int l, r;
+		cin >> l >> r;
+		count = 0;
+		for(int j=l-1; j<r-1; j++)
+			if(s.at(j) == s.at(j+1)){
+				count++;
+			}
+		cout << count << endl;
+	}
+
+
+	cout << endl;
+	return 0;
+}
