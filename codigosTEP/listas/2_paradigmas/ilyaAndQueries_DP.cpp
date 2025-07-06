b7ee1f432c6e4dd4058ef96301e370fe4c64ef0d
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-07-06 16:11:12 -0300

    Continuação da tentativa de DP

--- codigosTEP/listas/2_paradigmas/ilyaAndQueries_DP.cpp
+++ codigosTEP/listas/2_paradigmas/ilyaAndQueries_DP.cpp
@@ -5,49 +5,58 @@
 
 using namespace std;
 
-#define MAX 500
+#define MAX 20
 
-int table[MAX];
-int count=0; 
+int table[MAX], count=0;
+void print(const int table[]);
 
 int dp(string s, int index){
-	if(table[index] == -1){ //ainda nao verificado
+	if(table[index] == -1){
 		if(s.at(index) == s.at(index+1)){
+			cout << "index:" << index << endl;
+			count++;
 			return index;
-		}else{
+		}else if(s.at(index) != s.at(index+1)){
 			return 0;
 		}
 	}
 
-	if(table[index] == 0) //ja verificado
+	if(table[index] == 0){
 		return 0;
+	}
 
-	return 0;
+	return table[index];
 } 
 
 int main(){
 	string s;
-	int m;
+	int m, ans;
 
 	memset(table, -1, sizeof(table));
+	print(table);
 
 	cin >> s >> m;
 
 	for(int i=0; i<m; i++){
 		int l, r;
 		cin >> l >> r;
+		count = 0;
 		for(int j=l-1; j<r-1; j++){
-			if(dp(s, j) != 0){ 
-				count++;  
-				table[j] = dp(s, j);
-				cout << "j:" << j << endl;
-			}else{
-				table[j] = 0;
-			}	
+			ans = dp(s, j);	
+			table[j] = ans;
 		}
 		cout << count << endl;
 	}
+	print(table);
 
 	cout << endl;
 	return 0;
+}
+
+void print(const int table[]){
+	printf("\n\n");
+	for(int i=0; i<MAX; i++){
+		printf("%d ", table[i]);
+	}
+	printf("\n\n");
 }
