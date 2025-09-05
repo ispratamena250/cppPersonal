05bc8f6869e86cbdd15df3ab42604d01bd6e55c5
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-09-05 16:13:50 -0300

    Complex: começo de atividade de sala

---
+++ complex/zAndCNumbers_interations.cpp
@@ -0,0 +1,22 @@
+/*
+* This is a class exercise.
+* Let z and c complex numbers, k and n integers where k = 1, 2, ... and n = 0, 1, 2, ....
+* k is the index of the c number and n is the index of the z number.
+* If n = 0 then z0 = (0, 0).
+* If n > 0 then zn+1 = (zn)^2 + ck.
+* Iterate and save ck for o plotting.
+* Remember: abs(z) = sqrt(x^2 + y^2)
+* 
+* First of all, let's interate up to n = 101. If so abs(z101) < 2?
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    
+
+    cout << endl;
+    return 0;
+}
