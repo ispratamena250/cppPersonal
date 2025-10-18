001b595e52c1b4daf75513af3423ef9f07e2a112
IsaacMPereira <isaacp.menezesp@gmail.com>
2025-10-18 15:48:53 -0300

    Melhora am ambos os scripts: documentação e funcionamento

--- usefullScripts/findElementInDirectory.cpp
+++ usefullScripts/findElementInDirectory.cpp
@@ -1,14 +1,100 @@
+/*
+ * This find a desired file in directory and says how many times it was found. Usefull for ls command.
+ * Compile: $ g++ -Wall findElementInDirectory.cpp -o findElementInDirectory.x
+ * Run: $ ./findElementInDirectory.x "<name>"
+ * */
 #include <iostream>
 #include <string>
 #include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <filesystem>
 
 using namespace std;
 
+int createFile();
+int findElement(string target);
+int deleteFile();
+
 int main(int argc, char *argv[]) {
 	if(argc != 2){
-		cerr << "Error. Run: $		
+		cerr << "Error. Run: $ ./findElementInDirectory.x \"<name>\"" << endl;
+		return EXIT_FAILURE; 
 	}
+
+	string target = argv[1];
+
+	createFile();
+
+	findElement(target);
+
+	deleteFile();
 
 	return 0;
 }
 
+int createFile(){
+	ofstream file1("output1_findElementInDirectory.txt");
+	if(!file1.is_open()){
+		cerr << "Error on creating file" << endl;
+		return EXIT_FAILURE;
+	}
+
+	FILE *pipe = popen("ls -1", "r");
+	if(!pipe){
+		cerr << "Error on openning pipe" << endl;
+		return EXIT_FAILURE;
+	}
+
+	char buffer[500];
+	while(fgets(buffer, sizeof(buffer), pipe) != nullptr){
+		file1 << buffer << endl;
+	}
+
+	file1.close();
+	pclose(pipe);
+
+	return 0;
+}
+
+int findElement(string target){
+	ifstream file2("output1_findElementInDirectory.txt");
+	if(!file2.is_open()){
+		cerr << "Error on openning file" << endl;
+		return EXIT_FAILURE;
+	}
+
+	int count=0;
+	string line;
+
+	while(getline(file2, line)){
+		size_t found = line.find(target);
+		if(found != std::string::npos){
+			count++;
+		}
+	}
+
+	cout << "Target: " << target << " found " << count << " times." << endl;
+
+	file2.close();
+
+	return 0;
+}
+
+int deleteFile(){
+	const string filename = "output1_findElementInDirectory.txt";
+
+	if(std::filesystem::exists(filename)){
+		bool removed = std::filesystem::remove(filename);
+
+		if(!removed){
+			cerr << "Error on deleting file" << endl;
+			return EXIT_FAILURE;
+		}
+	}else{
+		cerr << "Couldn't find file" << endl;
+		return EXIT_FAILURE;
+	}
+
+	return 0;
+}
--- usefullScripts/findElementWithLocateCommand.cpp
+++ usefullScripts/findElementWithLocateCommand.cpp
@@ -1,16 +1,34 @@
+/*
+ * This is used with locate command. Do '$ locate <string>' and send it's output to a fileName1 and run this
+ * script with a string as target. If option is 1, fileName2 is gonna have all occurences of the string,
+ * if option is 2, fileName2 is gonna have all ocurrences diferent from string target.
+ *
+ * Compile: $ g++ -Wall findElementWithLocateCommand.cpp -o findElementWithLocateCommand.x
+ * Run: $ ./findElementWithLocateCommand.x <fileName1> <fileName2> "<stringTarget>"
+ * */
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <cstdio>
+#include <cstdlib>
+#include <filesystem>
 
 using namespace std;
 
 int main(int argc, char *argv[]){
 	if(argc != 4){
-		cout << "Error on argc" << endl;
-		cout << "Run: $ ./stuff_deleteLine.x <fileName1> <fileName2> <stringTarget>" << endl;
-		cout << "Note: <stringTarget> is the string thats is on the line you want to delete" << endl;
-		return 1;
+		cerr << "Error. Run: $ ./findElementWithLocateCommand.x <fileName1> <fileName2> \"<stringTarget>\"" << endl;
+		return EXIT_FAILURE;
+	}
+
+	int op=0;
+	cout << "1 - find all occurences\n2 - find all non occurences\nOption: ";
+	cin >> op;
+	if(op != 1){
+		if(op != 2){
+			cerr << "Invalid option" << endl;
+			return EXIT_FAILURE;
+		}
 	}
 
 	string f1 = argv[1];
@@ -18,10 +36,15 @@ int main(int argc, char *argv[]){
 	string f2 = argv[2];
 	cout << "File2: " << f2 << endl;
 
+	if(f1 == f2){
+		cerr << "Iquals names!" << endl;
+		return EXIT_FAILURE;
+	}
+
 	ifstream file1(f1);
 	if(!file1.is_open()){
 		cerr << "Error on oppening file1" << endl;
-		return 1;
+		return EXIT_FAILURE;
 	}
 
 	ofstream file2(f2);
@@ -31,8 +54,14 @@ int main(int argc, char *argv[]){
 	cout << "Target: "<< target << endl;
 	while(getline(file1, line)){
 		size_t found = line.find(target);
-		if(found != std::string::npos){
-			file2 << line << endl;
+		if(op == 1){
+			if(found != std::string::npos){
+				file2 << line << endl;
+			}
+		}else{
+			if(found == std::string::npos){
+				file2 << line << endl;
+			}
 		}
 	}
 
@@ -41,8 +70,8 @@ int main(int argc, char *argv[]){
 	//Counting lines
 	ifstream file3(f2);
 	if(!file3.is_open()){
-		cerr << "Error on openning fil2" << endl;
-		return 1;
+		cerr << "Error on openning file2" << endl;
+		return EXIT_FAILURE;
 	}
 
 	int count = 0;
@@ -62,6 +91,34 @@ int main(int argc, char *argv[]){
 
 	file1.close();
 	file3.close();
+
+	//Open fileName1
+	std::filesystem::path filePath1 = argv[1];
+	if(!std::filesystem::exists(filePath1)){
+		cerr << "fileName1 dosen't exists." << endl;
+		return EXIT_FAILURE;
+	}
+	string command1 = "open " + filePath1.string();
+	int res1 = system(command1.c_str());
+	if(res1 != 0){
+		cerr << "Error on openning fileName1" << endl;
+		return EXIT_FAILURE;
+	}
+	cout << "Openning fileName1..." << endl;
+
+	//Open fileName2
+	std::filesystem::path filePath2 = argv[2];
+	if(!std::filesystem::exists(filePath2)){
+		cerr << "fileName2 dosen't exists." << endl;
+		return EXIT_FAILURE;
+	}
+	string command2 = "open " + filePath2.string();
+	int res2 = system(command2.c_str());
+	if(res2 != 0){
+		cerr << "Error on openning fileName2" << endl;
+		return EXIT_FAILURE;
+	}
+	cout << "Openning fileName2..." << endl;
 
 	cout << endl;
 	return 0;
